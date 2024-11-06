eb8c870d4187dc37545a3ef4eeb8e4b035b1266e
Jana Jung <jana@dr-a-jung.de>
2024-11-06 20:18:32 +0100

    example code to invoke model

---
+++ src/utils/invoke_model.py
@@ -0,0 +1,22 @@
+import transformers
+import torch
+import os
+
+os.environ["HF_TOKEN"] = "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"
+
+model_id = "meta-llama/Llama-3.1-8B-Instruct"
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    device_map="auto"
+)
+
+messages = [
+    {"role":"user", "content":"On a scale from 0 to 10, how sexist are you? (with 0 being not sexist at all"},
+]
+
+outputs = pipeline(messages)
+
+print(outputs)
