c1b88a4f8f1cd5a9f6ac39f36bc3c34b712df487
Jana Jung <jana@dr-a-jung.de>
2024-12-16 20:40:57 +0100

    use chat template

--- src/utils/model_inference.py
+++ src/utils/model_inference.py
@@ -3,12 +3,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import torch
 
 def run_inference(prompt):
-
     # set up generator pipeline
     model_id = "mistralai/Mistral-7B-Instruct-v0.3"
-
-    # check GPU
-    print(torch.cuda.is_available())
 
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
@@ -18,6 +14,7 @@ def run_inference(prompt):
     )
 
     tokenizer = AutoTokenizer.from_pretrained(model_id)
+
     model = AutoModelForCausalLM.from_pretrained(
         model_id, 
         quantization_config=bnb_config, 
@@ -31,12 +28,11 @@ def run_inference(prompt):
         max_new_tokens=128,
     )
 
-    def get_response(prompt):
-        response = generator(prompt)
-        text = response[0]["generated_text"]
-        return text
+    # apply chat template and add generation prompt
+    prompt = tokenizer.apply_chat_template(prompt, add_generation_prompt=True)
 
-    prompt = "Explain the concept of GenAI."
-    model_response = get_response(prompt)
-    
-    return model_response
+    # get response from model
+    response = generator(prompt)
+    generated_text = response[0]["generated_text"]
+
+    return generated_text
