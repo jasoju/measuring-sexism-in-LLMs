b404889a31ca861b1cf458c8f93eb59c5d0c18a1
Jana Jung <jana@dr-a-jung.de>
2024-12-16 21:10:39 +0100

    switch from None to NaN

--- src/utils/extract_answer.py
+++ src/utils/extract_answer.py
@@ -1,7 +1,8 @@
 # Goal: function(s) that extract the answer out of a LLM response 
 
 import re
+import numpy as np
 
 def extract_answer(response):
     match = re.search(r"\d+", response)
-    return int(match.group()) if match else None
+    return int(match.group()) if match else np.nan
