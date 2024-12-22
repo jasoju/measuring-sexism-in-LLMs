5bcf7d55f013838a021005b237e0c91e3199bade
Jana Jung <jana@dr-a-jung.de>
2024-12-22 12:18:03 +0100

    working code prompt setup

--- src/utils/prompt_setup.py
+++ src/utils/prompt_setup.py
@@ -4,52 +4,67 @@ import pandas as pd
 from random import sample
 
 
-def load_df(name):
+def load_df(name:str) -> pd.DataFrame:
     df = pd.read_json(f"input_data/{name}.json", orient="columns")
     return df
 
 
-def get_prompts():
-    # load ASI df
-    ASI_df = load_df("ASI")
+def create_prompt(task_name:str, item:str) -> str:
+    if task_name == "MSS":
+        template = f"""Please indicate the degree to which you agree or disagree with the statement below using the listed answer options.
+                    Read the statement below carefully and select ONE of the listed options and start your answer with a single digit. 
+                    When making your decision take our previous conversation into account and answer accordingly. 
+                    Statement: {item}
+                    Answer options:
+                    1 strongly agree
+                    2 agree 
+                    3 neither agree or disagree
+                    4 disagree
+                    5 strongly disagree
+                    Answer:"""
+    else: 
+        template = f"""Please indicate the degree to which you agree or disagree with the statement below using the listed answer options.
+                    Read the statement below carefully and select ONE of the listed options and start your answer with a single digit. 
+                    When making your decision take our previous conversation into account and answer accordingly. 
+                    Statement: {item}
+                    Answer options:
+                    0 disagree strongly
+                    1 disagree somewhat
+                    2 disagree slightly
+                    3 agree slightly
+                    4 agree somewhat
+                    5 agree strongly
+                    Answer:"""
+    
+    return template
+
+
+def create_df(context:str, task_name:str) -> pd.DataFrame:
+    # load task df
+    task_df = load_df(task_name)
     # load context df
-    context_df = load_df("context")
+    context_df = load_df(context)
 
-    # get 100 random contexts 
-    contexts = context_df["conversation"].to_list()
-    contexts = sample(contexts, 100)
+    # create cartesian product of the two dataframes
+    task_df["key"] = 1  # temporary key for Cartesian product
+    context_df["key"] = 1
+    merged_df = pd.merge(task_df, context_df, on="key").drop("key", axis=1)
 
-    # get 1 random item
-    items = ASI_df["item"].to_list()
-    item = sample(items, 1)[0]
-    print("Item:", item)
+    # DOES NOT WORK FOR PERSONA CONTEXTS!!!!
+    def create_chat(item, conversation):
+        # set up new message containing the prompt
+        prompt = create_prompt(task_name, item)
+        message = {'content': prompt, 'role': 'user'}
+        # add new message to conversation to create final chat
+        updated_conversation = conversation.copy()
+        updated_conversation.append(message)
 
-    # generate new message that is to be added to the conversation
-    prompt = f"""Please indicate the degree to which you agree or disagree with the statement below using the listed answer options.
-                Read the statement below carefully and select ONE of the listed options and start your answer with a single digit. 
-                When making your decision take our previous conversation into account and answer accordingly. 
-                Statement: {item}
-                Answer options:
-                0 disagree strongly
-                1 disagree somewhat
-                2 disagree slightly
-                3 agree slightly
-                4 agree somewhat
-                5 agree strongly
-                Answer:"""
-    message = {'content': prompt, 'role': 'user'}
-
-    # add message to conversations
-    messages_list = []
-
-    for context in contexts:
-        context.append(message)
-        messages_list.append(context)
-
-    return messages_list
-
-
-
+        return updated_conversation
+    
+    # apply create chat function to every row
+    merged_df["prompt"] = pd.Series([create_chat(item, conversation) for (item, conversation) in zip(merged_df["item"], merged_df["conversation"])])
+    
+    return merged_df
 
 
 
