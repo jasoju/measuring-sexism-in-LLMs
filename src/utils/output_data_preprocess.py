6b640688b38f4469b273908fbbe91d0fe391c81b
Jana Jung <jana@dr-a-jung.de>
2025-01-22 10:13:08 +0100

    adapt to also work for MSS data

--- src/utils/output_data_preprocess.py
+++ src/utils/output_data_preprocess.py
@@ -2,9 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-def reverse_answer(row:pd.Series):
+def reverse_answer(row:pd.Series, task:str) -> int:
+    if task=="MSS":
+        anchor = 6
+    else:
+        anchor = 5
+
     if np.isnan(row["answer"]):
         return row["answer"]
     if row["reversed"]:
-        return 5 - row["answer"]  
+        return anchor - row["answer"]  
     return row["answer"]  
