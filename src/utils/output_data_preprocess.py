b5c7ecd416e62df1b8998e12ee98c785b2171254
Jana Jung <jana@dr-a-jung.de>
2025-02-11 11:08:33 +0100

    context subset based on quartiles

--- src/utils/output_data_preprocess.py
+++ src/utils/output_data_preprocess.py
@@ -13,3 +13,12 @@ def reverse_answer(row:pd.Series, task:str) -> int:
     if row["reversed"]:
         return anchor - row["answer"]  
     return row["answer"]  
+
+
+def sample_from_quartiles(df:pd.DataFrame, quartiles:list, col="total", n=10):
+    q1 = df[df[col] <= quartiles[0]].sample(n=n, random_state=8).index
+    q2 = df[(df[col] > quartiles[0]) & (df[col] <= quartiles[1])].sample(n=n, random_state=8).index
+    q3 = df[(df[col] > quartiles[1]) & (df[col] <= quartiles[2])].sample(n=n, random_state=8).index
+    q4 = df[df[col] > quartiles[2]].sample(n=n, random_state=42).index
+    # returns one list containing all identifiers (question_id or persona_id) of the sampled contexts
+    return list(q1) + list(q2) + list(q3) + list(q4)
