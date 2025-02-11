6647748c73de82f4d72cb887b2e7e147a6183201
Jana Jung <jana@dr-a-jung.de>
2025-02-11 12:28:48 +0100

    context subset (also work if less than 10 in quantile)

--- src/utils/output_data_preprocess.py
+++ src/utils/output_data_preprocess.py
@@ -14,9 +14,13 @@ def reverse_answer(row:pd.Series, task:str) -> int:
 
 
 def sample_from_quartiles(df:pd.DataFrame, quartiles:list, col="total", n=10):
-    q1 = df[df[col] <= quartiles[0]].sample(n=n, random_state=8).index
-    q2 = df[(df[col] > quartiles[0]) & (df[col] <= quartiles[1])].sample(n=n, random_state=8).index
-    q3 = df[(df[col] > quartiles[1]) & (df[col] <= quartiles[2])].sample(n=n, random_state=8).index
-    q4 = df[df[col] > quartiles[2]].sample(n=n, random_state=42).index
+    q1 = df[df[col] <= quartiles[0]]
+    q2 = df[(df[col] > quartiles[0]) & (df[col] <= quartiles[1])]
+    q3 = df[(df[col] > quartiles[1]) & (df[col] <= quartiles[2])]
+    q4 = df[df[col] > quartiles[2]]
+
+    sampled_indices = []
+    for q in [q1, q2, q3, q4]:
+        sampled_indices.extend(q.sample(n=min(n, len(q)), random_state=8).index)  # if less then 10 in one quartile, just sample all
     # returns one list containing all identifiers (question_id or persona_id) of the sampled contexts
-    return list(q1) + list(q2) + list(q3) + list(q4)
+    return sampled_indices
