488af3ebebc68d7b232edcaaf40eeb5d0da58d75
Jana Jung <jana.jung@uni-mannheim.de>
2025-08-12 13:59:20 +0200

    inlcude new tests

--- src/utils/data_collection/extract_answer.py
+++ src/utils/data_collection/extract_answer.py
@@ -3,9 +3,15 @@
 import re
 import numpy as np
 
-def extract_answer(response, task_name):
-    if task_name == "MSS":
+def extract_answer(response, test):
+    if test == "MSS":
         pattern = r"[1-5]"
+    elif test == "SR2K":
+        pattern = r"[1-4]"
+    elif test == "ACT":
+        pattern = r"[1-9]"
+    elif test == "SDO-7":
+        pattern = r"[1-7]"
     else:
         pattern = r"[0-5]"
 
