1ab44ff56fcf51574ed9e6b80be4f3d32c358104
Jana Jung <jana.jung@uni-mannheim.de>
2025-08-22 15:23:31 +0200

    new prompt setup

--- src/utils/data_collection/prompt_setup.py
+++ src/utils/data_collection/prompt_setup.py
@@ -1,7 +1,6 @@
-# Goal: set up the list of messages based on context df and questionnaire df which are used as prompt
-
 import pandas as pd
 import random
+from typing import Union
 import re
 
 
@@ -13,76 +12,80 @@ def load_df(name:str|None) -> pd.DataFrame:
     return df
 
 
-def create_prompt(task_name:str|None, item: str, answer_options:list, random_options:bool, model_id:str) -> str:
+def create_prompt(
+        task_name:str|None, 
+        item: str, 
+        answer_options:list[str], 
+        instruction:str, 
+        random_options:bool, 
+        model_id:str,
+        change_sentence_end: bool
+) -> Union[str, list[dict[str, str]]]:
+    """
+    Create a model-ready prompt string or message list depending on the model type.
+    """
+
     if task_name == "ref_letter_generation":
         return item
     
+    # Copy options to avoid modifying the original list
+    options = answer_options.copy()
+    
     # wrap each option with << >> when using centaur
     if "Centaur" in model_id:   
-        answer_options = [f"<<{opt}>>" for opt in answer_options]
+        options = [f"<<{opt}>>" for opt in options]
 
     # shuffle the answer options if random == True
-    answer_options=answer_options.copy()
     if random_options:
-        random.shuffle(answer_options)
-    options_str = "\n ".join(answer_options)
+        random.shuffle(options)
+
+    # join answer options in single string
+    options_str = "\n ".join(options)
+
+    # ending based on change_sentence_end
+    sentence_end = "?" if change_sentence_end else ":"
 
     # create the prompt
-    prompt = f"""Please indicate the degree to which you agree or disagree with the statement below using the listed answer options.
+    prompt = f"""{instruction}
                 Read the statement below carefully and select ONE of the listed options and start your answer with a single digit. 
-                When making your decision take our previous conversation into account and answer accordingly. 
                 Statement: {item}
                 Answer options:
                 {options_str}
-                Answer:"""
+                Your answer{sentence_end}"""
+    
+    # create message list if model is not Centaur
+    if "Centaur" not in model_id:
+        prompt = [{'content': prompt, 'role': 'user'}]
 
     return prompt
 
 
-def create_df(context:str|None, task_name:str, random_options:bool, model_id:str) -> pd.DataFrame:
-    # load task df
-    task_df = load_df(task_name)
-    # load context df
-    if task_name == "ref_letter_generation" and (context in ["chatbot_arena_conv", "persona_hub"]):
-        model_name = re.search(r'[^/]+$', model_id).group(0)    # extract model name from model_id
-        context_df = load_df(f"{context}__subset__{model_name}")
-    else:
-        context_df = load_df(context)
-    # TEST SETTING: only use 10 first rows
-    # context_df = context_df.head(10)
+def create_df(task_name:str, 
+              random_options:bool, 
+              model_id:str, 
+              change_sentence_end: bool = False
+) -> pd.DataFrame:
+    """
+    Load a task dataframe and add a column with prompts.
+    """
 
-    if context is None:
-        merged_df = task_df.copy()
-    else:
-        # create cartesian product of the two dataframes
-        task_df["key"] = 1  # temporary key for Cartesian product
-        context_df["key"] = 1
-        merged_df = pd.merge(task_df, context_df, on="key").drop("key", axis=1)
+    df = load_df(task_name)
 
-    def create_message_list(item, context, answer_options):
-        # set up new message containing the prompt
-        prompt = create_prompt(task_name, item, answer_options, random_options, model_id)
-        message = {'content': prompt, 'role': 'user'}
-        # add new message to conversation to create final chat
-        if context is None:
-            message_list = []
-        else: 
-            message_list = context.copy()
-        message_list.append(message)
+    df["prompt"] = [
+        create_prompt(
+            task_name=task_name,
+            item=item,
+            answer_options=options,
+            instruction=instr,
+            random_options=random_options,
+            model_id=model_id,
+            change_sentence_end=change_sentence_end
+        )
+        for item, options, instr in zip(df["item"], df["answer_options"], df["instruction"])
+    ]
 
-        return message_list
-    
-    # apply create message list function to every row (input columns depend on context type)
-    if context is None or context == "random_state":
-        merged_df["prompt"] = pd.Series([create_message_list(item, None, answer_options) for item, answer_options in zip(merged_df["item"], merged_df["answer_options"])])
-    elif "chatbot_arena_conv" in context:
-        merged_df["prompt"] = pd.Series([create_message_list(item, conversation, answer_options) for (item, conversation, answer_options) in zip(merged_df["item"], merged_df["context"], merged_df["answer_options"])])
-    elif context == "persona_hub":
-        merged_df["prompt"] = pd.Series([create_message_list(item, persona, answer_options) for (item, persona , answer_options) in zip(merged_df["item"], merged_df["context"], merged_df["answer_options"])])
-    else:
-        raise ValueError(f"{context} as context type is not allowed.")
 
-    return merged_df
+    return df
 
 
 
