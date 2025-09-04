85d66a795142f916aae8c3931b96e3ebe513a306
Jana Jung <jana.jung@uni-mannheim.de>
2025-09-04 15:43:30 +0200

    bug fix eval sample

--- src/reliability_eval.py
+++ src/reliability_eval.py
@@ -9,16 +9,19 @@ output_data_dir = os.path.join(current_dir,"src", "output_data")
 results_dir = os.path.join(current_dir,"results")
 
 # all tasks & versions we need/want a reliability evaluation for
-tasks = ["ASI"]   # "ASI", "SR2K", "MFQ"
+tasks = ["ASI", "SR2K", "MFQ"]   # "ASI", "SR2K", "MFQ"
 versions = ["af", "reversed", "changed_eos"]
+
+# beginn list with all dfs
+all_dfs = []
 
 for task in tasks: 
     # load og data  
     df_og = load_and_concat_jsons(base_dir=output_data_dir, subfolder=task, file_suffix=task)
     df_og["version"] = "og"
 
-    # beginn list with all dfs
-    all_dfs = [df_og]
+    # append og versin
+    all_dfs.append(df_og)
 
     for version in versions:
         # load version a´data
@@ -33,8 +36,8 @@ for task in tasks:
                                   output_dir=os.path.join(results_dir,task))
 
 
-    # save random sample (across og & all versions) for each model for evalaution of answer extraction
-    save_sample_for_eval(df=pd.concat(all_dfs, ignore_index=True), n=100, dir=os.path.join(current_dir,"evaluation"))
+# save random sample (across og & all versions) for each model for evalaution of answer extraction
+save_sample_for_eval(df=pd.concat(all_dfs, ignore_index=True), n=100, dir=os.path.join(current_dir,"evaluation"))
 
 
 
